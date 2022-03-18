5b9627bf8a121430bd709cfe1dd40ab89c6f2564
AtienoObwanda <atienoobwanda@gmail.com>
2022-03-18 03:06:09 +0300

    Added contact form, pizza gallery

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
 // Animations init
 new WOW().init();
+
+//FORM ACTION
+$(document).ready(function(){
+    $("form").submit(function(event){
+      event.preventDefault();
+      var name = $("input#nam").val();
+      alert("Hello " + name + ", we have received your message! Thanks for contacting us. We'll get back to  you soon");
+      //refresh page
+      $('#contactform').trigger("reset");
+    });
+  });
